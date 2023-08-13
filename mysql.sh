f490ee7ff15aa3926d90f522f71e5ac45e518d5a
cproc <syscproc@gmail.com>
2023-08-13 19:08:42 +0630

    add mysql install shell script

---
+++ mysql.sh
@@ -0,0 +1,7 @@
+sudo apt install default-libmysqlclient-dev -y
+sudo apt install default-libmysqld-dev -y
+sudo apt install default-mysql-client -y
+sudo apt install default-mysql-client-core -y
+sudo apt install default-mysql-server -y
+sudo apt install default-mysql-server-core -y
+sudo apt install mysql-common -y
